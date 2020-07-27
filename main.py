83cdcc39b6b123824f486a42b975dfd9ba9086b9
jweir136 <31812897+jweir136@users.noreply.github.com>
2020-07-27 14:54:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-########### IMPORT THE REQUIREMENTS ##########
+########### IMPORT THE 3RD PARTY REQUIREMENTS ##########
 import tensorflow.keras as keras
 import cv2
 import os
+
+########### IMPORT THE CUSTOM REQUIREMENTS #############
+
+print(os.listdir("storage/projects"))
